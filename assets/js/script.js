143b47a84701359615468f93dacb2e6186ae8b7c
miaciasMAC <miaciasullo@gmail.com>
2022-12-27 16:57:23 -0500

    set up HTML to accept JS weather data. formatted main-weather-card to bootstrap image overlay card style

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,12 @@ const queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&
 - auto update interval: every 10 minutes
 - recommended to represent weather with central point in territory instead of all 200,000+ cities
 - calling more than once per 10 min on free plan will auto-suspend key (code 429: blocked account)
+
+ERRORS
+- 401 - did not specify api key in request, wrong key, or fetching disallowed info (paid vs unpaid subscription)
+- 404 - wrong city name, ZIP, or city ID. or wrong request format
+- 429 - surpassing limit of free subscription
+- 500, 502, 503, 504 - CONTACT OpenWeather via email with example of api request that failed
 */
 
 fetch(queryUrl);
